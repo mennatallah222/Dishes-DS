5b6e5b4e7f2970249384ffa676fc1728a99ab93a
mennatallah222 <mennatallahahmed892@gmail.com>
2025-05-04 02:44:08 +0300

    fix: connecting the java ee with postgres

--- dishes-proj/admin-services/src/main/java/com/dishes/entities/Admin.java
+++ dishes-proj/admin-services/src/main/java/com/dishes/entities/Admin.java
@@ -1,8 +1,11 @@
 package com.dishes.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 
 @Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Admin extends User {
 
 }
--- dishes-proj/admin-services/src/main/java/com/dishes/entities/CompanyRep.java
+++ dishes-proj/admin-services/src/main/java/com/dishes/entities/CompanyRep.java
@@ -1,8 +1,11 @@
 package com.dishes.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 
 @Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class CompanyRep extends User {
     private String companyName;
     public String getCompanyName() {
